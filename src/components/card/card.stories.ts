6f0de5605aa2d77513867603b710bf356ec4637b
RafaLindemann <lindemannrafael@icloud.com>
2021-02-09 17:39:27 +0100

    api integration

--- src/components/card/card.stories.ts
+++ src/components/card/card.stories.ts
@@ -1,30 +1,57 @@
 import "./card.css";
 import { createCard } from "./card";
 import { createElement } from "../../utils/createElement";
+// import { getCharacter } from "../../utils/api";
+import { getCharacter, getCharacters } from "../../utils/api";
 
 export default {
   title: "Components/Card",
   parameters: { layout: "centered" },
 };
 
+export const Rick = () =>
+  createCard({
+    imgSrc: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+    name: "Rick Sanchez",
+    status: "Alive",
+    species: "Human",
+    origin: "Earth (C-137)",
+  });
+
+export const Morty = () =>
+  createCard({
+    imgSrc: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+    name: "Morty Smith",
+    species: "Human",
+    origin: "Earth (C-137)",
+    status: "Alive",
+  });
+
+export const Armothy = () =>
+  createCard({
+    imgSrc: "https://rickandmortyapi.com/api/character/avatar/25.jpeg",
+    name: "Armothy",
+    status: "Dead",
+    species: "unknown",
+    origin: { name: "Post-Apocalyptic Earth" },
+  });
+
 export const Multiple = () => {
   const characters = [
     {
-      imgSrc: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-      name: "Rick Sanchez",
-      species: "Human",
-      origin: "Earth (C-137)",
-      status: "Alive",
-    },
-
-    {
       imgSrc: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
       name: "Morty Smith",
+      status: "Dead",
       species: "Human",
-      origin: "Earth (C-137)",
-      status: "Alive",
+      origin: { name: "Earth (C-137)" },
     },
-
+    {
+      imgSrc: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+      name: "Rick Sanchenz",
+      status: "Alive",
+      species: "Human",
+      origin: { name: "Earth (C-137)" },
+    },
     {
       imgSrc: "https://rickandmortyapi.com/api/character/avatar/25.jpeg",
       name: "Armothy",
@@ -37,5 +64,30 @@ export const Multiple = () => {
     className: "container",
     childs: characters.map((character) => createCard(character)),
   });
+
   return container;
 };
+
+export const CharacterFromAPI = (args, { loaded: { character } }) => {
+  return createCard(character);
+};
+
+CharacterFromAPI.loaders = [
+  async () => ({
+    character: await getCharacter(401),
+  }),
+];
+
+export const CharactersFromAPI = (args, { loaded: { characters } }) => {
+  const container = createElement("div", {
+    className: "container",
+    childs: characters.map((character) => createCard(character)),
+  });
+  return container;
+};
+
+CharactersFromAPI.loaders = [
+  async () => ({
+    characters: await getCharacters(),
+  }),
+];
